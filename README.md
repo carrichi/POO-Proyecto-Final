c32f279052dbcdbd668ef30d6a0ed36c2904e364
RobertoCarrichi <robertocarrichiunam@gmail.com>
2021-01-28 05:46:59 -0600

    feat: Comentarios iniciales en el archivo principal

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 # https://github.com/RobertoCarrichi/POO-Proyecto-Final
 
-En este proyecto se pondrán en práctica todos los temas vistos durante el curso de Programación Orientada a Objetos. Para esto se realizará un programa que simule la administración de una empresa editorial de revistas científicas. 
+En este proyecto se pondrán en práctica todos los temas vistos durante el curso de Programación Orientada a Objetos. Para esto se realizará un programa que simule la administración de una empresa editorial de revistas científicas.
--- Start.java
+++ Start.java
@@ -2,5 +2,29 @@
  * Esta es la clase que realizara la ejecucion de todo el sistema de administracion.
  */
 public class Start {
-    
+    /**
+     * Metodo que realiza el comienzo de todo el programa.
+     * @param args Indica los argumentos que puede recibir desde la consola de comandos.
+     */
+    public static void main(String[] args) {
+        /*
+        Al inicial el programa se requiere realizar un LOGIN y buscar si los datos son correctos.
+        */
+        
+        /*
+            Despues de que se haya registrado, es necesario mostrarle un menu me opciones segun el usuario que haya
+            iniciado sesion.
+
+            Este sera un ciclo que no terminara hasta que el usuario actual decida cerrar su sesión.
+        */
+
+        boolean exit = false;
+        while ( ! exit ) {
+
+
+            
+            exit = true;
+        }
+
+    }
 }
