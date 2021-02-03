d54b06ee11029c5d162e73688adc1a099b69ec4d
MirandaBueno <54423136+MirandaBueno@users.noreply.github.com>
2021-02-03 10:52:54 -0600

    Carpeta de Visitor

---
+++ PatronVisitor/Visitable.java
@@ -0,0 +1,5 @@
+package PatronVisitor;
+
+public interface Visitable{
+	public void aceptar(Visitor visitor);
+}
---
+++ PatronVisitor/Visitor.java
@@ -0,0 +1,17 @@
+package PatronVisitor;
+
+import Personas.Subscriptores.*;
+import Personas.Empleados.*;
+import Revistas.*;
+
+public interface Visitor{
+	public void visit(Revisor revisor);
+	public void visit(Editor editor);
+	public void visit(Autor autor);
+	public void visit(Subscriptor sub);
+	//public void visit(Empleado empleado);
+	public void visit(Articulo articulo);
+	public void visit(Categoria cat);
+	public void visit(Revista revista);
+	
+}
