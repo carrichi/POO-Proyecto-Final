b3e3332a3ac04867c9acb69b9f8f4bf53e3e86e0
MirandaBueno <54423136+MirandaBueno@users.noreply.github.com>
2021-02-06 02:41:09 -0600

    Update Categoria.java

--- Revistas/Categoria.java
+++ Revistas/Categoria.java
@@ -21,29 +21,51 @@ public class Categoria implements Serializable,Visitable {
      */
     private static final long serialVersionUID = 1L;
     
+/**Nombre de la categoria
+*/
     private String nombre;
+	
+	/**Articulos de esta categoria
+	*/
     private ArrayList<Articulo> articulos;
-
+	
+	/**Regresa el nombre de la categoria
+	* @return nombre
+	*/
     public String getNombre() {
         return nombre;
     }
-
+	
+	/**Guarda el nombre de la categoria
+	* @param nombre
+	*/
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
+    	
+	/** Regresa la lista de los articulos de esta categoria
+	* @return articulos
+	*/
     public ArrayList<Articulo> getArticulos() {
         return articulos;
     }
-
+	/** Guarda la lista de articulos de esta categoria
+	* @param articulos
+	*/
     public void setArticulos(ArrayList<Articulo> articulos) {
         this.articulos = articulos;
     }
 
+	/** Return cantidad de articulos de la categoria
+	* @return int
+	*/
     public int getNumArticulos() {
         return this.articulos.size();
     }
     
+	/**Implementa el patron de diseño Visitor 
+     * @param visitor Visitor especifico
+     */
     public void aceptar(Visitor visitor){
 		visitor.visit(this);
 	}
